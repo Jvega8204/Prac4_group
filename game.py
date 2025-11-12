0364c06665c825bfc4e1de76aa5a1d6f1f260926
Vraj Shah <vshah9270@floridapoly.edu>
2025-11-12 22:01:38 +0000

    Add computer random choice feature

--- game.py
+++ game.py
@@ -1,3 +1,9 @@
 print("welcome to Rock-Paper-Scissors!")  
-player_choice = input("Enter Rock, Paper, or Scissors: ").title()
-print(f"You chose{player_choice}")
+player_choice = input("Enter Rock, Paper, or scissors: ").title()
+print(f"You chose {player_choice}")
+
+import random
+
+options = ["Rock", "Paper", "Scissors"]
+computer_choice = random.choice(options)
+print("Computer chooses:", computer_choice)
